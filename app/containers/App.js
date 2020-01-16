ee614c7a44fef9b9441ca65e7408058c9fc86fc3
Andrew Chang <achang97@stanford.edu>
2020-01-16 10:44:51 -0800

    Added basic functionality for reading HTML content of a page.

--- app/containers/App.js
+++ app/containers/App.js
@@ -35,37 +35,53 @@ export default class App extends Component {
     };
 
     this.listener = this.listener.bind(this);
+    this.handleFirstPageLoad = this.handleFirstPageLoad.bind(this);
   }
 
   componentDidMount() {
     chrome.runtime.onMessage.addListener(this.listener);
-
-    window.onload = () => {
-      this.handleTabUpdate(window.location.href);
-    }
+    window.addEventListener('load', this.handleFirstPageLoad);
   }
 
   componentWillUnmount() {
     chrome.runtime.onMessage.removeListener(this.listener);
+    window.removeEventListener('load', this.handleFirstPageLoad);
   }
 
   getPageText() {
-    // TODO: Basic version that is website agnostic, simply removes chrome extension code from DOM
-    // and gets inner text. Future version should look at specific divs (ie. title and content of email for Gmail).
+    // TODO: Basic version that is website agnostic, simply removes chrome extension code, scripts,
+    // and styles from DOM and gets inner text. Future version should look at specific divs (ie. title
+    // and content of email for Gmail).
+    const docCopy = document.cloneNode(true);
     
-    // const allDivs = document.body.querySelectorAll("*:not(#omni-chrome-ext-main-container)");
-    // let pageText = '';
-    // allDivs.forEach(div => pageText += `${div.innerText}\n`);
-    // return pageText;
+    const omniExt = docCopy.getElementById("omni-chrome-ext-main-container");
+    omniExt.remove();
+    
+    const removeSelectors = ["script", "noscript", "style"];
+    removeSelectors.forEach(selector => {
+      const elements = docCopy.querySelectorAll(`body ${selector}`);
+      for (const elem of elements) {
+        elem.remove();
+      }
+    })
+
+    return docCopy.body.innerText;
+  }
+
+  handleFirstPageLoad() {
+    this.handleTabUpdate(window.location.href);
   }
 
   handleTabUpdate(url) {
     // Placeholder code for AI Suggest, code should be written in another file eventually
     // Case 1: Matches 
+    const text = this.getPageText();
+    console.log(text);
+      
     if (/https:\/\/mail\.google\.com\/mail\/u\/\d+\/#inbox\/.+/.test(url)) {
       this.setState({ suggestTabVisible: true });
-      // const text = this.getPageText();
-      // console.log(text)
+      const text = this.getPageText();
+      console.log(text)
     } else {
       this.setState({ suggestTabVisible: false });
     }
