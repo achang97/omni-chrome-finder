dfbf85106cf0a688b384fdcb1b0c127b1ded3618
chetankr <chet2an@gmail.com>
2020-01-16 18:58:51 -0800

    got the card to open resize and drag + styling

--- app/containers/Cards/index.js
+++ app/containers/Cards/index.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import style from './cards.css';
 import Draggable from 'react-draggable';
+import { Resizable } from "re-resizable";
 import _ from 'underscore';
 
 import CloseIcon from '@material-ui/icons/Close';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import CheckIcon from '@material-ui/icons/Check';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
+import { Button } from '@material-ui/core';
+
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -71,20 +78,64 @@ export default class App extends Component {
     return (
       <div>
         <style type="text/css">{style}</style>
+        
         { cards.map(card => (
           <Draggable
             bounds="html"
+            handle="strong"
             // positionOffset={{x: '20%', y: '20%'}}
             position={positions[card]}
             onDrag={(e, ui) => this.handleDrag(card, ui)}
           >
-            <div className="card padder-lg white-background">
-              <button onClick={() => closeCard(card)}>
-                <CloseIcon />
-              </button>
-              <div> Card ID: {card} </div>
-            </div>
+            <Resizable className="card white-background"
+              defaultSize={{
+                width:500,
+                height:500,
+              }} minWidth={500} minHeight={500}
+              enable={ {top:false, right:true, bottom:true, left:false, topRight:false, bottomRight:true, bottomLeft:false, topLeft:false} } >
+              <div className="card-top-section">
+                <strong className="card-header padder-horizontal-lg padder-top-lg padder-bottom-md flex-row-centered flex-justify-space-between">
+                  <div>2 Days Ago</div>
+                  <div className="flex-row-centered">
+                    <MoreHorizIcon />
+                    <div onClick={() => closeCard(card)}>
+                      <CloseIcon />
+                    </div>
+                  </div>
+                </strong>
+                <div className="padder-horizontal-lg padder-bottom-lg">
+                  <div className="card-title">How do I delete a user?</div>
+                  <div className="margin-vertical-lg">Here is the answer on how to do that. This should eventually be in a rich text editor, but we'll deal with that later. </div>
+                  <div className="flex-row-centered flex-justify-space-between">
+                    <div className="flex-row-centered">
+                      <div className="flex-row-centered padder-sm card-tag-container margin-right-sm">
+                        <div className="margin-right-sm">Customer Request Actions</div>
+                      </div> 
+                      <div className="flex-row-centered padder-sm card-tag-container">
+                        <div className="margin-right-sm">Onboarding</div>
+                      </div> 
+                    </div>
+
+                    <div className="card-flag-up-to-date flex-row-centered padder-sm"> 
+                      <CheckIcon className="margin-right-sm" />
+                      <div>Up To Date</div>
+                      <ArrowDropDownIcon />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+
+              <div className="card-bottom-panel full-width flex-row-centered flex-justify-space-between">
+                <Button>
+                  Edit Card
+                </Button>
+              </div>
+
+            </Resizable>
           </Draggable>
+          
         ))}
       </div>
     );
