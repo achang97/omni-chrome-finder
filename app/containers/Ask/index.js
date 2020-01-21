26ee91bc3a1f7e04e8a382499ea58e667e97c9e1
Mustafa Haider <mustafi5199@gmail.com>
2020-01-21 21:39:03 +0500

    add tailwind css

--- app/containers/Ask/index.js
+++ app/containers/Ask/index.js
@@ -1,73 +1,74 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { TextField } from '@material-ui/core';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-
 import style from './ask.css';
-
 
 import { openCard } from '../../actions/display';
 
 @connect(
+    // eslint-disable-next-line no-unused-vars
   state => ({
   }),
-  dispatch => bindActionCreators({
-  	openCard,
-  }, dispatch)
+  dispatch => bindActionCreators({ openCard, }, dispatch)
 )
 
 export default class Ask extends Component {
-	constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
       tabValue: 0,
-    }
-
-    this.handleTabClick = this.handleTabClick.bind(this);
+    };
   }
 
-  handleTabClick(event, tabValue) {
+  handleTabClick = (event, tabValue) => {
     this.setState({ tabValue });
 
-    let path;
     switch (tabValue) {
       case 0:
-        path = '/ask';
-        break;
+        return '/ask';
       case 1:
-        path = '/create';
-        break;
+        return '/create';
       case 2:
-        path = '/navigate';
-        break;
+        return '/navigate';
       default:
-        return
+        return;
     }
 
     //this.props.history.push(path);
   }
 
   openCard() {
+      // eslint-disable-next-line no-tabs,no-mixed-spaces-and-tabs,react/prop-types
   	this.props.openCard(Math.floor(Math.random() * Math.floor(10000)));
   }
 
   render() {
-  	const { tabValue } = this.state;
+    const { tabValue } = this.state;
     return (
       <div>
-      	<style type="text/css">{style}</style>
-      	<div className="padder-lg">
-      		<Tabs value={tabValue} onChange={this.handleTabClick} TabIndicatorProps={{ style: { display: 'none', }}}>
-            <Tab label="Slack" className="integrations-tab" />
-            <Tab label="Email" className="integrations-tab" />
-            <Tab label="Asana" className="integrations-tab" />
-          </Tabs>
-      		<TextField id="standard-basic" className="question-text-field" placeholder="Question" InputProps={{ disableUnderline: true}} fullWidth={true}/>
-        	<div>Ask Body</div>
-        	<button onClick={() => this.openCard()}> Open Card </button>
+        <style type="text/css">{style}</style>
+        <div className="padder-lg">
+          <div className="flex float-left w-full">
+            <div className="w-2/3">
+              <Tabs value={tabValue} onChange={this.handleTabClick} TabIndicatorProps={{ style: { display: 'none', } }}>
+                <Tab label="Slack" className="integrations-tab shadow-md" />
+                <Tab label="Email" className="integrations-tab shadow-md" />
+                <Tab label="Asana" className="integrations-tab shadow-md" />
+              </Tabs>
+            </div>
+          </div>
+          <TextField id="standard-basic" className="question-text-field w-full" placeholder="Question" InputProps={{ disableUnderline: true }} />
+          <div>Ask Body</div>
+          <button
+            className="bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
+            onClick={() => this.openCard()}
+          >
+              Open Card
+          </button>
         </div>
       </div>
     );
