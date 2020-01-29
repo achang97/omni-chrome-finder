ed8bf34dad91f0d2771ca3c1640735be77ce113e
chetankr <chet2an@gmail.com>
2020-01-28 18:01:34 -0800

    Fix overflow issue on cards

--- app/components/cards/CardContent/index.js
+++ app/components/cards/CardContent/index.js
@@ -76,20 +76,20 @@ export default class CardContent extends Component {
   renderFooter = () => {
   	const { id, isEditing } = this.props;
   	return (
-  		<div>
+  		<div className={s("flex-shrink-0 min-h-0")}>
   			{
   			isEditing ?
 
   			<Button
   				text={"Save Changes"}
   				onClickButton={() => this.saveCard(id)}
-  				buttonClassName={s("fixed bottom-0 left-0 right-0 rounded-t-none p-lg")}
+  				buttonClassName={s("rounded-t-none p-lg")}
   				underline
   			/>
 
   			
 	        :
-	        <div className={s("flex items-center justify-between fixed bottom-0 bg-purple-light rounded-b-lg p-lg left-0 right-0")}>      
+	        <div className={s("flex items-center justify-between bg-purple-light rounded-b-lg p-lg")}>     
 	          <Button 
 	          	text={"Edit Card"} 
 	          	icon={<MdModeEdit className={s("mr-sm")} />} 
@@ -118,102 +118,104 @@ export default class CardContent extends Component {
     const { id, isEditing, answerEditorState } = this.props;
     const { descriptionEditorEnabled, answerEditorEnabled } = this.state;
     return (
-      <div>
-        <div className={s("bg-purple-light p-sm")}>
-          <strong className={s("text-xs text-purple-reg px-lg pt-lg pb-sm flex items-center justify-between opacity-75")}>
-            <div>2 Days Ago</div>
-            <div className={s("flex items-center")}>
-              <MdMoreHoriz />
-            </div>
-          </strong>
-          <div className={s("px-lg pb-lg")}>
-            <div className={s("text-2xl font-semibold")}>How do I delete a user? ({id}) </div>
-
-            { isEditing ?
-
-            	
-            		descriptionEditorEnabled ?
-            		<TextEditor 
-            			onEditorStateChange={this.onDescriptionEditorStateChange} 
-            			editorState={this.state.descriptionEditorState} 
-	        			wrapperClassName={s('card-description-text-editor-wrapper-edit')} 
-	        			editorClassName={s('text-editor')} 
-	        			toolbarClassName={s('text-editor-toolbar')}
-	        			autoFocus />
- 					:
- 					<div onClick={() => this.enableDescriptionEditor()} >
-	            		<TextEditor
-	            			onEditorStateChange={this.onDescriptionEditorStateChange} 
-	            			editorState={this.state.descriptionEditorState}
-	        				wrapperClassName={s('card-description-text-editor-wrapper-inactive cursor-pointer')} 
-	        				editorClassName={s('text-editor card-description-text-editor-view')} 
-	        				toolbarClassName={s('')}
-	        				toolbarHidden
-	        				readOnly/>
-        			</div>
-        		: 
-        		<TextEditor 
-        			onEditorStateChange={this.onDescriptionEditorStateChange} 
-        			editorState={this.state.descriptionEditorState} 
-        			wrapperClassName={s('text-editor-wrapper card-description-text-editor-wrapper-view ')} 
-        			editorClassName={s('text-editor card-description-text-editor-view')} 
-        			toolbarClassName={''} 
-        			toolbarHidden
-        			readOnly/>
-            }
-
-            { !isEditing &&
-	            <div className={s("flex items-center justify-between")}>
-	              <div className={s("flex items-center")}>
-	                { ['Customer Request Actions', 'Onboarding'].map(tag => (
-	                  <div key={tag} className={s("flex items-center p-xs mr-xs bg-purple-grey text-purple-reg rounded-full font-semibold text-xs")}>
-	                    <div className={s("mr-xs")}>Customer Request Actions</div>
-	                  </div> 
-	                ))}
-	              </div>
-	              <div className={s("flex items-center p-xs bg-green-xlight text-green-reg rounded-lg font-semibold text-xs")}> 
-	                <MdCheck className={s("mr-xs")} />
-	                <div>Up To Date</div>
-	                <MdArrowDropDown />
-	              </div>
+      <div className={s("flex-grow flex flex-col min-h-0")}>
+      	<div className={s("flex-grow flex flex-col min-h-0")}>
+	        <div className={s("bg-purple-light p-sm flex-grow min-h-0 overflow-scroll")}>
+	          <strong className={s("text-xs text-purple-reg px-lg pt-lg pb-sm flex items-center justify-between opacity-75")}>
+	            <div>2 Days Ago</div>
+	            <div className={s("flex items-center")}>
+	              <MdMoreHoriz />
 	            </div>
-			}
-          </div>
-        </div>
-        <div className={s('p-2xl')}>
-	        { isEditing ?
+	          </strong>
+	          <div className={s("px-lg pb-lg")}>
+	            <div className={s("text-2xl font-semibold")}>How do I delete a user? ({id}) </div>
 
-	        	answerEditorEnabled ?
-		        	<TextEditor 
-		        		onEditorStateChange={this.onAnswerEditorStateChange} 
-		        		editorState={this.state.answerEditorState} 
-		        		wrapperClassName={'card-answer-text-editor-wrapper-edit'} 
-		        		editorClassName={'text-editor'} 
-		        		toolbarClassName={'text-editor-toolbar'}
-		        		autoFocus />
-		        	:
-		        	<div onClick={() => this.enableAnswerEditor()} >
+	            { isEditing ?
+
+	            	
+	            		descriptionEditorEnabled ?
+	            		<TextEditor 
+	            			onEditorStateChange={this.onDescriptionEditorStateChange} 
+	            			editorState={this.state.descriptionEditorState} 
+		        			wrapperClassName={s('card-description-text-editor-wrapper-edit')} 
+		        			editorClassName={s('text-editor')} 
+		        			toolbarClassName={s('text-editor-toolbar')}
+		        			autoFocus />
+	 					:
+	 					<div onClick={() => this.enableDescriptionEditor()} >
+		            		<TextEditor
+		            			onEditorStateChange={this.onDescriptionEditorStateChange} 
+		            			editorState={this.state.descriptionEditorState}
+		        				wrapperClassName={s('card-description-text-editor-wrapper-inactive cursor-pointer')} 
+		        				editorClassName={s('text-editor card-description-text-editor-view')} 
+		        				toolbarClassName={s('')}
+		        				toolbarHidden
+		        				readOnly/>
+	        			</div>
+	        		: 
+	        		<TextEditor 
+	        			onEditorStateChange={this.onDescriptionEditorStateChange} 
+	        			editorState={this.state.descriptionEditorState} 
+	        			wrapperClassName={s('text-editor-wrapper card-description-text-editor-wrapper-view ')} 
+	        			editorClassName={s('text-editor card-description-text-editor-view')} 
+	        			toolbarClassName={''} 
+	        			toolbarHidden
+	        			readOnly/>
+	            }
+
+	            { !isEditing &&
+		            <div className={s("flex items-center justify-between")}>
+		              <div className={s("flex items-center")}>
+		                { ['Customer Request Actions', 'Onboarding'].map(tag => (
+		                  <div key={tag} className={s("flex items-center p-xs mr-xs bg-purple-grey text-purple-reg rounded-full font-semibold text-xs")}>
+		                    <div className={s("mr-xs")}>Customer Request Actions</div>
+		                  </div> 
+		                ))}
+		              </div>
+		              <div className={s("flex items-center p-xs bg-green-xlight text-green-reg rounded-lg font-semibold text-xs")}> 
+		                <MdCheck className={s("mr-xs")} />
+		                <div>Up To Date</div>
+		                <MdArrowDropDown />
+		              </div>
+		            </div>
+				}
+	          </div>
+	        </div>
+	        <div className={s('p-2xl flex-grow min-h-0 overflow-scroll')}>
+		        { isEditing ?
+
+		        	answerEditorEnabled ?
 			        	<TextEditor 
 			        		onEditorStateChange={this.onAnswerEditorStateChange} 
 			        		editorState={this.state.answerEditorState} 
-			        		wrapperClassName={'card-answer-text-editor-wrapper-inactive cursor-pointer'} 
-			        		editorClassName={'text-editor card-answer-text-editor-view'} 
-			        		toolbarClassName={s('')}
-		        			toolbarHidden
-		        			readOnly
-		        			/>
-	        		</div>
-	        	:
-	        	<TextEditor 
-	        		onEditorStateChange={this.onAnswerEditorStateChange} 
-	        		editorState={this.state.answerEditorState} 
-	        		wrapperClassName={'text-editor-wrapper'} 
-	        		editorClassName={'text-editor card-answer-text-editor-view'} 
-	        		toolbarClassName={''} 
-	        		toolbarHidden
-	        		readOnly
-	        		/>
-	        }
+			        		wrapperClassName={'card-answer-text-editor-wrapper-edit'} 
+			        		editorClassName={'text-editor'} 
+			        		toolbarClassName={'text-editor-toolbar'}
+			        		autoFocus />
+			        	:
+			        	<div onClick={() => this.enableAnswerEditor()} >
+				        	<TextEditor 
+				        		onEditorStateChange={this.onAnswerEditorStateChange} 
+				        		editorState={this.state.answerEditorState} 
+				        		wrapperClassName={'card-answer-text-editor-wrapper-inactive cursor-pointer'} 
+				        		editorClassName={'text-editor card-answer-text-editor-view'} 
+				        		toolbarClassName={s('')}
+			        			toolbarHidden
+			        			readOnly
+			        			/>
+		        		</div>
+		        	:
+		        	<TextEditor 
+		        		onEditorStateChange={this.onAnswerEditorStateChange} 
+		        		editorState={this.state.answerEditorState} 
+		        		wrapperClassName={'text-editor-wrapper'} 
+		        		editorClassName={'text-editor card-answer-text-editor-view'} 
+		        		toolbarClassName={''} 
+		        		toolbarHidden
+		        		readOnly
+		        		/>
+		        }
+	        </div>
         </div>
         { this.renderFooter() }
       </div>
