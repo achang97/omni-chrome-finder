c22ae9bfe6db6f70111d281a5e156706d26bc709
Andrew Chang <achang97@stanford.edu>
2020-04-22 23:06:34 -0700

    Fixed screen recording open behavior

--- app/middleware/screenRecording.js
+++ app/middleware/screenRecording.js
@@ -1,6 +1,6 @@
 import * as types from 'actions/actionTypes';
 import { clearScreenRecording } from 'actions/screenRecording';
-import { toggleDock } from 'actions/display';
+import { toggleDock, expandDock } from 'actions/display';
 
 const cardsMiddleware = store => next => (action) => {
   const nextAction = next(action);
@@ -8,17 +8,21 @@ const cardsMiddleware = store => next => (action) => {
 
   switch (type) {
     case types.END_SCREEN_RECORDING: {
-      const {
-        screenRecording: { recording, onSuccess, activeId },
-        display: { dockVisible }
-      } = store.getState();
+      const { screenRecording: { recording, onSuccess, activeId } } = store.getState();
 
       onSuccess({ recording });
       store.dispatch(clearScreenRecording());
+      break;
+    }
+    case types.CLEAR_SCREEN_RECORDING: {
+      const { display: { dockVisible } } = store.getState();
 
       if (!dockVisible) {
         store.dispatch(toggleDock());
       }
+
+      // Expand in all cases (not technically accurate but ok for now)
+      store.dispatch(expandDock());
       break;
     }
     default: {
