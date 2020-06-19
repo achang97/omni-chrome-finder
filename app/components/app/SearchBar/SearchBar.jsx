e18d7d506e95e0005594588e0d21eee4e1e2c5b2
chetankr <chet2an@gmail.com>
2020-06-18 20:28:31 -0700

    Restyle searchbar
    1. Add Omni logo
    2. Add settings button
    3. Remove toggle in with X

--- app/components/app/SearchBar/SearchBar.jsx
+++ app/components/app/SearchBar/SearchBar.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDebouncedCallback } from 'use-debounce';
-import { MdChevronRight } from 'react-icons/md';
+import { MdSettings, MdClose } from 'react-icons/md';
 import { ANIMATE, ROUTES } from 'appConstants';
 import Dock from 'react-dock';
 
+import logo from 'assets/images/logos/logo-dark-icon.svg';
 import { getStyleApplicationFn } from 'utils/style';
 import { DOCK_PANEL_STYLE } from 'styles/dock';
+import style from './search-bar.css';
 
 const DOCK_WIDTH = 225;
 
-const s = getStyleApplicationFn();
+const s = getStyleApplicationFn(style);
 
 const SearchBar = ({
   onlyShowSearchBar,
@@ -46,21 +48,30 @@ const SearchBar = ({
         height: 'auto'
       }}
     >
-      <div className={s('flex')}>
+      <div className={s('flex items-center')}>
+        {/*
         <div
           className={s(
-            'self-stretch px-xs rounded-l-lg bg-purple-light text-purple-reg flex items-center cursor-pointer'
+            'self-stretch px-xs rounded-l-lg bg-purple-reg text-white flex items-center cursor-pointer'
           )}
           onClick={minimizeSearchBar}
         >
           <MdChevronRight />
+        </div> */}
+
+        <div className={s('toggle-close-button')} onClick={minimizeSearchBar}>
+          <MdClose />
         </div>
+        <img src={logo} alt="Omni logo for searchbar" className={s('searchbar-logo ml-sm')} />
         <input
           onChange={(e) => updateAskSearchText(e.target.value)}
           value={searchText}
-          placeholder="Search Omni"
-          className={s('flex-1 search-input m-xs')}
+          placeholder="Search in Omni"
+          className={s('flex-1 searchbar-input m-sm')}
         />
+        <div className={s('text-gray-reg')}>
+          <MdSettings className={s('text-xs mr-xs')} />
+        </div>
       </div>
     </Dock>
   );
